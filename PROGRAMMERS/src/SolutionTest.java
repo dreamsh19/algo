64aebfaf162fda97f850e602ca382cbfd89a266b
dreamsh19 <dreamsh19@snu.ac.kr>
2020-09-23 00:21:35 +0900

    Add SolutionTest

---
+++ PROGRAMMERS/src/SolutionTest.java
@@ -0,0 +1,27 @@
+public class SolutionTest {
+
+    static void printIntArray(int[] arr){
+        String s = "[";
+        for(int a:arr){
+            s+=a+",";
+        }
+        s=s.substring(0,s.length()-1)+"]";
+        System.out.println(s);
+    }
+    public static void main(String[] args) {
+        int T;
+
+        //Solution1
+        T = 3;
+        int[] brown = {10, 8, 24};
+        int[] yellow = {2, 1, 24};
+        Solution1 sol = new Solution1();
+        for (int i = 0; i < T; i++) {
+            int[] ans = sol.solution(brown[i],yellow[i]);
+            printIntArray(ans);
+        }
+
+
+    }
+
+}
