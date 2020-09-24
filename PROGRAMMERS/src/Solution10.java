a5af8101ecf0e30274acdabb45a62c283cc40014
dreamsh19 <dreamsh19@snu.ac.kr>
2020-09-25 07:31:27 +0900

    Refactor #12

--- PROGRAMMERS/src/Solution10.java
+++ PROGRAMMERS/src/Solution10.java
@@ -2,17 +2,15 @@ import java.util.Arrays;
 
 public class Solution10 {
     public int solution(int[][] routes) {
-        Arrays.sort(routes, (o1, o2) -> o1[0] - o2[0]);
+        Arrays.sort(routes, (o1, o2) -> o1[1] - o2[1]);
         int ans = 0;
         int end = Integer.MIN_VALUE;
 
-        for (int i = 0; i < routes.length; i++) {
-            int from = routes[i][0];
-            int to = routes[i][1];
+        for (int[] route:routes) {
+            int from = route[0];
+            int to = route[1];
             if (from > end) {
                 ans++;
-                end = to;
-            } else if (to < end) {
                 end = to;
             }
         }
