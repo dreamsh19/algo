3fac9e3f7409b9a06c6cf1c9f60670e731956841
dreamsh19 <dreamsh19@snu.ac.kr>
2020-09-26 01:58:19 +0900

    Improve #17 - Efficiency test passed

--- PROGRAMMERS/src/Solution15.java
+++ PROGRAMMERS/src/Solution15.java
@@ -1,6 +1,5 @@
 public class Solution15 {
     public String solution(String number, int k) {
-//        System.out.printf("NUMBER[%s]k[%d]\n", number, k);
         if (k == 0) return number;
         if (number.length() <= k) return "";
 
@@ -8,6 +7,10 @@ public class Solution15 {
         int maxIdx = 0;
         char max = '0';
         for (int i = 0; i <= k; i++) {
+            if (numArr[i] == '9') {
+                maxIdx = i;
+                break;
+            }
             if (numArr[i] > max) {
                 max = numArr[i];
                 maxIdx = i;
@@ -16,7 +19,7 @@ public class Solution15 {
         if (maxIdx == 0) {
             return numArr[0] + solution(number.substring(1), k);
         } else {
-            return solution(number.substring(maxIdx), k - maxIdx);
+            return numArr[maxIdx] + solution(number.substring(maxIdx + 1), k - maxIdx);
         }
     }
 }
