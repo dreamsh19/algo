1715dba7045a29fc5cf8d822075e8e95cd22e258
dreamsh19 <dreamsh19@snu.ac.kr>
2020-09-25 22:28:10 +0900

    Refactor #14 - Reverse order

--- PROGRAMMERS/src/Solution12.java
+++ PROGRAMMERS/src/Solution12.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 
 public class Solution12 {
     public int solution(int[] citations) {
-        int answer = 0;
+
         int l = citations.length;
         Arrays.sort(citations);
-        for (int i = 1; i <= l; i++) {
+        for (int i = l; i > 0; i--) {
             int min = citations[l - i];
-            if (min < i) return i - 1;
+            if (min >= i) return i;
         }
-        return l;
+        return 0;
     }
 }
