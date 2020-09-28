7e5b1ad88e7adb42cb5874c315e104b9f8aa5611
dreamsh19 <dreamsh19@snu.ac.kr>
2020-09-28 21:40:03 +0900

    Implement #22

---
+++ PROGRAMMERS/src/Solution20.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Solution20 {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int n = people.length;
+        int light = 0, heavy = n - 1;
+        while(light<=heavy){
+
+            if(light==heavy){
+                answer++;
+                break;
+            }
+
+            if(people[light]+people[heavy] <= limit){
+                light++;
+                heavy--;
+                answer++;
+            }else{
+                heavy--;
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
