f019a8979bbd215791334be04a40bab4eacd95cd
dreamsh19 <dreamsh19@snu.ac.kr>
2020-09-28 20:56:32 +0900

    Re-implement #20 - Use graph & bfs

--- PROGRAMMERS/src/Solution18.java
+++ PROGRAMMERS/src/Solution18.java
@@ -1,8 +1,12 @@
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution18 {
 
-    HashMap<String, Boolean> visited;
+    int[] dist;
+    boolean[] visited;
+    Queue<Integer> q;
+    LinkedList<Integer>[] adjList;
 
     public int getDiff(String s1, String s2) {
         int cnt = 0;
@@ -14,36 +18,58 @@ public class Solution18 {
         return cnt;
     }
 
+    void addEdge(int i, int j) {
+        adjList[i].add(j);
+        adjList[j].add(i);
+    }
+
     public int solution(String begin, String target, String[] words) {
-        if (visited == null) {
-            visited = new HashMap<>();
-            for (String w : words) {
-                visited.put(w, false);
+        q = new LinkedList<>();
+        int n = words.length;
+        dist = new int[n];
+        visited = new boolean[n];
+        adjList = new LinkedList[n];
+        for (int i = 0; i < n; i++) {
+            dist[i] = Integer.MAX_VALUE;
+            adjList[i] = new LinkedList<>();
+        }
+
+        int target_idx = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (words[i].equals(target)) {
+                target_idx = i;
+                break;
             }
-            boolean isPossible = false;
-            for (String w : words) {
-                if (w.equals(target)) {
-                    isPossible = true;
-                    break;
+        }
+        if (target_idx == -1) return 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (getDiff(words[i], words[j]) == 1) {
+                    addEdge(i, j);
                 }
             }
-            if (!isPossible) return 0;
-
         }
 
-        if (begin.equals(target)) return 0;
-
-        int answer = Integer.MAX_VALUE;
-        visited.put(begin, true);
-        for (String w : words) {
-            if (!visited.get(w) && getDiff(w, begin) == 1) {
-                int s = solution(w, target, words);
-                if (s >= 0) answer = Math.min(answer, 1 + s);
+        for (int i = 0; i < n; i++) {
+            if (getDiff(words[i], begin) == 1) {
+                dist[i] = 1;
+                q.offer(i);
+                visited[i] = true;
             }
         }
-
-        if (answer == Integer.MAX_VALUE) return -1;
-
-        return answer;
+        while (!q.isEmpty()) {
+            int next = q.poll();
+            if (target_idx == next) return dist[target_idx];
+            for (int adj : adjList[next]) {
+                if (!visited[adj]) {
+                    q.offer(adj);
+                    visited[adj] = true;
+                    dist[adj] = dist[next] + 1; // dist = height
+                }
+            }
+        }
+        return visited[target_idx] ? dist[target_idx] : 0;
     }
 }
