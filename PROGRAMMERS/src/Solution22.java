8eeb6cdd1db94a3b4e341e70999d6defa226efa2
dreamsh19 <dreamsh19@snu.ac.kr>
2020-09-30 00:22:53 +0900

    Refactor #24 - Remove redundant isWall & count

--- PROGRAMMERS/src/Solution22.java
+++ PROGRAMMERS/src/Solution22.java
@@ -6,16 +6,8 @@ public class Solution22 {
 
     public int solution(int m, int n, int[][] puddles) {
 
-        isWall = new boolean[m + 2][n + 2];
-        count = new int[m + 2][n + 2];
-        for (int i = 1; i <= m; i++) {
-            isWall[i][0] = true;
-            isWall[i][n + 1] = true;
-        }
-        for (int j = 1; j <= n; j++) {
-            isWall[0][j] = true;
-            isWall[m + 1][j] = true;
-        }
+        isWall = new boolean[m + 1][n + 1];
+        count = new int[m + 1][n + 1];
 
         for (int[] puddle : puddles) {
             int x = puddle[0];
