3e6d18d3467b0c75f3590a9a3e0871403db15356
dreamsh19 <dreamsh19@snu.ac.kr>
2020-10-05 03:22:54 +0900

    Refactor #28

--- PROGRAMMERS/src/Solution26.java
+++ PROGRAMMERS/src/Solution26.java
@@ -21,24 +21,25 @@ public class Solution26 {
 
     int MST(int start) {
         int len = 0;
+        dist = new int[n];
         pq = new PriorityQueue<>((o1, o2) -> dist[o1] - dist[o2]);
         inMST = new boolean[n];
         for (int i = 0; i < n; i++) {
             dist[i] = Integer.MAX_VALUE;
         }
         dist[start] = 0;
-        for (int i = 0; i < n; i++) {
-            pq.add(i);
-        }
+        pq.add(start);
         while (!pq.isEmpty()) {
             int poll = pq.remove();
             inMST[poll] = true;
             len += dist[poll];
             for (Node adj : adjList[poll]) {
-                if (!inMST[adj.idx] && adj.val < dist[adj.idx]) {
-                    dist[adj.idx] = adj.val;
-                    pq.remove(adj.idx);
-                    pq.add(adj.idx);
+                int idx = adj.idx;
+                int val = adj.val;
+                if (!inMST[idx] && val < dist[idx]) {
+                    dist[idx] = val;
+                    pq.remove(idx);
+                    pq.add(idx);
                 }
             }
         }
@@ -49,7 +50,6 @@ public class Solution26 {
     public int solution(int n, int[][] costs) {
         this.n = n;
         adjList = new LinkedList[n];
-        dist = new int[n];
         for (int i = 0; i < n; i++) {
             adjList[i] = new LinkedList<>();
         }
@@ -61,10 +61,6 @@ public class Solution26 {
             adjList[b].add(new Node(a, c));
         }
 
-//        int answer = Integer.MAX_VALUE;
-//        for (int i = 0; i < n; i++) {
-//            answer = Math.min(answer, MST(i));
-//        }
         return MST(0);
     }
 }
