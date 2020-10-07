9b7983b18250c0756972906fd777908aec0266d6
dreamsh19 <dreamsh19@snu.ac.kr>
2020-10-07 15:17:27 +0900

    Refactor #29 - Use stack

--- PROGRAMMERS/src/Solution27.java
+++ PROGRAMMERS/src/Solution27.java
@@ -1,62 +1,67 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Stack;
 
 public class Solution27 {
-    HashMap<String, ArrayList<Node>> adjList;
-    ArrayList<String> ans;
+
+    static final boolean FINISH = true;
+    static final String START = "ICN";
+    HashMap<String, ArrayList<Edge>> adjList;
+    Stack<Edge> st;
     int len;
 
-    class Node {
-        String to;
-        boolean visited;
 
-        Node(String to) {
+    class Edge {
+        String to;
+        boolean inStack;
+
+        Edge(String to) {
             this.to = to;
-            this.visited = false;
+            this.inStack = false;
         }
     }
 
-    void dfs(String start) {
-        if (ans.size() == len + 1) return;
-        ans.add(start);
-        for (Node adjNode : adjList.get(start)) {
-            if (!adjNode.visited) {
-                adjNode.visited = true;
-                dfs(adjNode.to);
-                if (ans.size() == len + 1) break;
-                ans.remove(ans.size() - 1);
-                adjNode.visited = false;
+    boolean dfs(String start) {
+        if (st.size() == len) return FINISH;
+
+        for (Edge e : adjList.get(start)) {
+            if (!e.inStack) {
+                e.inStack = true;
+                st.push(e);
+                if (dfs(e.to) == FINISH) return FINISH;
+                st.pop();
+                e.inStack = false;
             }
         }
-
-
+        return !FINISH;
     }
 
 
     public String[] solution(String[][] tickets) {
         len = tickets.length;
         adjList = new HashMap<>();
-        ans = new ArrayList<>();
+        st = new Stack<>();
+
         for (String[] ticket : tickets) {
             String from = ticket[0];
             String to = ticket[1];
-            ArrayList<Node> tmp = adjList.getOrDefault(from, new ArrayList<>());
-            tmp.add(new Node(to));
-            adjList.put(from, tmp);
+            adjList.putIfAbsent(from, new ArrayList<>());
+            adjList.get(from).add(new Edge(to));
             adjList.putIfAbsent(to, new ArrayList<>());
         }
         for (String key : adjList.keySet()) {
-            ArrayList<Node> tmp = adjList.get(key);
+            ArrayList<Edge> tmp = adjList.get(key);
             Collections.sort(tmp, (o1, o2) -> o1.to.compareTo(o2.to));
         }
 
-        dfs("ICN");
+        dfs(START);
 
         String[] answer = new String[len + 1];
-        for (int i = 0; i <= len; i++) {
-            answer[i] = ans.get(i);
+        for (int i = len; i >= 1; i--) {
+            answer[i] = st.pop().to;
         }
+        answer[0] = START;
 
         return answer;
     }
