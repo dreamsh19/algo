1a265e138404794a699dda777a4e36b33ee752fb
dreamsh19 <dreamsh19@snu.ac.kr>
2020-10-09 19:08:11 +0900

    Refactor #11 - Use Arrays.copyOfRange()

--- PROGRAMMERS/src/Solution9.java
+++ PROGRAMMERS/src/Solution9.java
@@ -1,18 +1,14 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class Solution9 {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         int ans_idx = 0;
         for (int[] command : commands) {
-            ArrayList<Integer> tmp = new ArrayList<>();
-            int from = command[0]-1;
-            int to = command[1]-1;
             int k = command[2];
-            for(int i=from;i<=to;i++) tmp.add(array[i]);
-            Collections.sort(tmp);
-            answer[ans_idx++]=tmp.get(k-1);
+            int[] tmp = Arrays.copyOfRange(array, command[0] - 1, command[1]);
+            Arrays.sort(tmp);
+            answer[ans_idx++] = tmp[k - 1];
 
         }
         return answer;
