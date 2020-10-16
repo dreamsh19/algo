0e1d246843f82618e9317f7323de3a624c85aa41
dreamsh19 <dreamsh19@snu.ac.kr>
2020-10-16 19:33:29 +0900

    Re-implement #47 - Use bfs & OOP

--- BAEKJOON/src/Main8.java
+++ BAEKJOON/src/Main8.java
@@ -1,162 +1,208 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main8 {
 
     int N, M;
-    static final int RED = -2, BLUE = -3, HOLE = 1;
-    static final int HAS_NONE = 0, HAS_R = 1, HAS_B = 2, HAS_RB = 3;
-    static final int MAX_DEPTH = 10;
-    int[][] matrix;
-    int ans;
+    static final char WALL = '#', EMPTY = '.', HOLE = 'O';
+    static final int MAX_TIME = 10;
+    char[][][] matrices;
 
-    void rotate() {
-        transpose();
-        flip();
+    class State {
+
+        static final int NULL = -1;
+
+        int idx;
+        int rx, ry, bx, by;
+        int time;
+
+        State() {
+
+        }
+
+        State(int idx, int rx, int ry, int bx, int by, int time) {
+            this.idx = idx;
+            this.rx = rx;
+            this.ry = ry;
+            this.bx = bx;
+            this.by = by;
+            this.time = time;
+        }
+
+        void setRed(int x, int y) {
+            rx = x;
+            ry = y;
+        }
+
+        void setBlue(int x, int y) {
+            bx = x;
+            by = y;
+        }
+
+        State rotate() {
+            int nIdx = (idx + 1) % 4;
+            int m = matrices[idx].length;
+            int nrx = ry;
+            int nry = m - 1 - rx;
+            int nbx = by;
+            int nby = m - 1 - bx;
+            return new State(nIdx, nrx, nry, nbx, nby, time);
+        }
+
+        boolean isRed(int x, int y) {
+            return x == rx && y == ry;
+        }
+
+        boolean isBlue(int x, int y) {
+            return x == bx && y == by;
+        }
+
+        void pushDown() {
+            if (rx > bx) {
+                pushRedDown();
+                pushBlueDown();
+            } else {
+                pushBlueDown();
+                pushRedDown();
+            }
+            time++;
+        }
+
+        void pushRedDown() {
+            char[][] matrix = matrices[idx];
+            int m = matrix.length;
+            while (++rx < m && matrix[rx][ry] != WALL && !isBlue(rx, ry)) {
+                if (matrix[rx][ry] == HOLE) {
+                    setRed(NULL, NULL);
+                    return;
+                }
+            }
+            rx--;
+        }
+
+        void pushBlueDown() {
+            char[][] matrix = matrices[idx];
+            int m = matrix.length;
+            while (++bx < m && matrix[bx][by] != WALL && !isRed(bx, by)) {
+                if (matrix[bx][by] == HOLE) {
+                    setBlue(NULL, NULL);
+                    return;
+                }
+            }
+            bx--;
+        }
+
+        boolean hasRed() {
+            return rx != NULL;
+        }
+
+        boolean hasBlue() {
+            return bx != NULL;
+        }
+
+        public State clone() {
+            return new State(idx, rx, ry, bx, by, time);
+        }
+
+        @Override
+        public String toString() {
+            return "State{" +
+                    "idx=" + idx +
+                    ", rx=" + rx +
+                    ", ry=" + ry +
+                    ", bx=" + bx +
+                    ", by=" + by +
+                    ", time=" + time +
+                    '}';
+        }
     }
 
-    void transpose() {
+    //
+    void getRotates() {
+        for (int i = 0; i < 3; i++) {
+            char[][] tmp = transpose(matrices[i]);
+            flip(tmp);
+            matrices[i + 1] = tmp;
+        }
+    }
+
+    char[][] transpose(char[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int[][] tmp = new int[m][n];
+        char[][] tmp = new char[m][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 tmp[j][i] = matrix[i][j];
             }
         }
-        matrix = tmp;
+        return tmp;
     }
 
-    void flip() {
+    void flip(char[][] matrix) {
         int m = matrix[0].length;
-        for (int[] row : matrix) {
+        for (char[] row : matrix) {
             for (int j = 0; j < m / 2; j++) {
-                int tmp = row[j];
+                char tmp = row[j];
                 row[j] = row[m - 1 - j];
                 row[m - 1 - j] = tmp;
             }
         }
     }
 
-    void goToBottom(int x, int y) {
-        int num = matrix[x][y];
-        int n = matrix.length;
-        int next_x = x;
-        while (next_x + 1 < n && matrix[next_x + 1][y] >= 0) {
-            next_x++;
-            if (matrix[next_x][y] == HOLE) {
-                matrix[x][y] = 0;
-                return;
+    int solution() {
+        State initialState = getInitialState();
+        getRotates();
+        Queue<State> bfsQ = new LinkedList<>();
+        bfsQ.offer(initialState);
+        while (!bfsQ.isEmpty()) {
+            State s = bfsQ.poll();
+            for (int i = 0; i < 4; i++) {
+                State ns = s.clone();
+                ns.pushDown();
+                if (ns.hasBlue()) {
+                    if (!ns.hasRed()) return ns.time;
+                    else if (ns.time < MAX_TIME) bfsQ.offer(ns);
+                }
+                s = s.rotate();
             }
         }
-        matrix[x][y] = 0;
-        matrix[next_x][y] = num;
 
+        return -1;
     }
 
-    int check() {
-        boolean hasRed = false, hasBlue = false;
-        for (int[] row : matrix) {
-            for (int i : row) {
-                if (i == RED) hasRed = true;
-                if (i == BLUE) hasBlue = true;
-            }
-        }
-        return hasRed ? (hasBlue ? HAS_RB : HAS_R) : (hasBlue ? HAS_B : HAS_NONE);
-    }
 
-    void pushDown() {
-        for (int k = 0; k < 2; k++) {
-            int rx = -1, ry = -1, bx = -1, by = -1;
-            int n = matrix.length;
-            int m = matrix[0].length;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (matrix[i][j] == RED) {
-                        rx = i;
-                        ry = j;
-                    }
-                    if (matrix[i][j] == BLUE) {
-                        bx = i;
-                        by = j;
-                    }
+    State getInitialState() {
+        State initialState = new State();
+        char[][] matrix = matrices[0];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (matrix[i][j] == 'R') {
+                    initialState.setRed(i, j);
+                    matrix[i][j] = EMPTY;
+                } else if (matrix[i][j] == 'B') {
+                    initialState.setBlue(i, j);
+                    matrix[i][j] = EMPTY;
                 }
             }
-            if (rx != -1) goToBottom(rx, ry);
-            if (bx != -1) goToBottom(bx, by);
         }
+        return initialState;
     }
 
-    void copyMatrix(int[][] from, int[][] to) {
-        int n = from.length;
-        int m = from[0].length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                to[i][j] = from[i][j];
-            }
-        }
-    }
-
-    void operation(int depth) {
-        if (depth + 1 >= ans || depth == MAX_DEPTH) {
-            return;
-        }
-        for (int i = 0; i < 4; i++) {
-            int n = matrix.length;
-            int m = matrix[0].length;
-            int[][] tmp = new int[n][m];
-            copyMatrix(matrix, tmp);
-            pushDown();
-            int result = check();
-            if (result == HAS_B) {
-                ans = Math.min(ans, depth + 1);
-            } else if (result == HAS_RB) {
-                operation(depth + 1);
-            }
-            matrix = tmp;
-            rotate();
-        }
-    }
-
-    void printMatrix() {
-        String s = "";
-        for (int[] row : matrix) s += Arrays.toString(row) + '\n';
-        System.out.println(s);
-    }
-
-    int solution() {
-        ans = Integer.MAX_VALUE;
-        operation(0);
-        return ans == Integer.MAX_VALUE ? -1 : ans;
-    }
 
     void getInput() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] token = br.readLine().split(" ");
         N = Integer.parseInt(token[0]);
         M = Integer.parseInt(token[1]);
-        matrix = new int[N][M];
+        char[][] matrix = new char[N][M];
         for (int i = 0; i < N; i++) {
-            char[] line = br.readLine().toCharArray();
-            for (int j = 0; j < M; j++) {
-                char c = line[j];
-                int tmp;
-                if (c == '#') {
-                    tmp = -1;
-                } else if (c == 'R') {
-                    tmp = RED;
-                } else if (c == 'B') {
-                    tmp = BLUE;
-                } else if (c == 'O') {
-                    tmp = HOLE;
-                } else {
-                    tmp = 0;
-                }
-                matrix[i][j] = tmp;
-            }
+            matrix[i] = br.readLine().toCharArray();
         }
+        matrices = new char[4][][];
+        matrices[0] = matrix;
     }
 
     public static void main(String[] args) throws IOException {
