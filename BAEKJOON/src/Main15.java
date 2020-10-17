c4243d6fba11a8379058c6a2d8ae31c46b44ec4a
dreamsh19 <dreamsh19@snu.ac.kr>
2020-10-17 20:44:01 +0900

    Another implement #54 - Use dp

--- BAEKJOON/src/Main15.java
+++ BAEKJOON/src/Main15.java
@@ -44,11 +44,24 @@ public class Main15 {
         return combination(new int[N], 0, 0);
     }
 
+    int solution_() {
+        int[] dp = new int[N + 1];
+        dp[N] = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            int t = time[i];
+            int p = price[i];
+
+            if (i + t > N) dp[i] = dp[i + 1];
+            else dp[i] = Math.max(dp[i + 1], dp[i + t] + p);
+        }   
+        return dp[0];
+    }
+
 
     public static void main(String[] args) {
         Main15 m = new Main15();
         m.getInput();
-        int ans = m.solution();
+        int ans = m.solution_();
         System.out.println(ans);
     }
 }
