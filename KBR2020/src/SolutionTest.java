e9e84897128c51f3e6bf5980143bacbcfb1ea77f
dreamsh19 <dreamsh19@snu.ac.kr>
2020-10-21 02:03:12 +0900

    Re-implement #59 - Remove redundant critical lock

--- KBR2020/src/SolutionTest.java
+++ KBR2020/src/SolutionTest.java
@@ -21,11 +21,18 @@ public class SolutionTest {
 //            print(s.solution(ss[i]));
 //        }
 
-        //Solution2
-        T = 3;
-        String[] p = {"(()())()", ")(", "()))((()"};
-        Solution2 s = new Solution2();
-        for (int i = 0; i < T; i++) print(s.solution(p[i]));
+//        //Solution2
+//        T = 3;
+//        String[] p = {"(()())()", ")(", "()))((()"};
+//        Solution2 s = new Solution2();
+//        for (int i = 0; i < T; i++) print(s.solution(p[i]));
+
+        //Solution1
+        T = 1;
+        int[][][] key = {{{0, 0, 0}, {1, 0, 0}, {0, 1, 1}}};
+        int[][][] lock = {{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}}};
+        Solution3 s = new Solution3();
+        for (int i = 0; i < T; i++) print(s.solution(key[i], lock[i]));
 
     }
 }
