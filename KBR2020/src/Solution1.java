986b877d60c0c88dae5ddf8494a3f27b9bd50c84
dreamsh19 <dreamsh19@snu.ac.kr>
2020-10-20 22:47:12 +0900

    Refactor #57 - Use recursive function getZipLen()

--- KBR2020/src/Solution1.java
+++ KBR2020/src/Solution1.java
@@ -1,34 +1,26 @@
-
 class Solution1 {
+
+    int getZipLen(String s, int tokSize) {
+        int len = s.length();
+        if (len < tokSize) return len;
+
+        String token = s.substring(0, tokSize);
+        int count = 1;
+        int i;
+        for (i = tokSize; i + tokSize <= len; i += tokSize) {
+            if (token.equals(s.substring(i, i + tokSize))) count++;
+            else break;
+        }
+        return (count == 1 ? 0 : (int) (Math.log10(count) + 1)) + tokSize + getZipLen(s.substring(i), tokSize);
+    }
+
+
     public int solution(String s) {
         int len = s.length();
         int answer = len;
 
-        for (int token_size = 1; token_size <= len / 2; token_size++) {
-            int ans_temp = 0;
-            int start = 0;
-            String token = s.substring(start, start + token_size);
-            String next = token;
-            while (true) {
-                int count = 0;
-                while (token.equals(next)) {
-                    count++;
-                    start += token_size;
-                    if (start + token_size > len) break;
-                    next = s.substring(start, start + token_size);
-                }
-                token = next;
-                if (count > 1) {
-                    ans_temp += token_size + (int) (Math.log10(count) + 1);
-                } else {
-                    ans_temp += token_size;
-                }
-                if (start + token_size > len){
-                    ans_temp += len-start;
-                    break;
-                }
-            }
-            answer = Math.min(ans_temp, answer);
+        for (int tokSize = 1; tokSize <= len / 2; tokSize++) {
+            answer = Math.min(answer, getZipLen(s, tokSize));
         }
         return answer;
     }
