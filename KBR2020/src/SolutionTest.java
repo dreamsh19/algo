cb72a04577d816c981978ff0bfa62740db962916
dreamsh19 <dreamsh19@snu.ac.kr>
2020-10-21 16:48:57 +0900

    Refactor #60 - Integrate ROW and COL & Add isRemovable()

--- KBR2020/src/SolutionTest.java
+++ KBR2020/src/SolutionTest.java
@@ -27,12 +27,27 @@ public class SolutionTest {
 //        Solution2 s = new Solution2();
 //        for (int i = 0; i < T; i++) print(s.solution(p[i]));
 
-        //Solution1
-        T = 1;
-        int[][][] key = {{{0, 0, 0}, {1, 0, 0}, {0, 1, 1}}};
-        int[][][] lock = {{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}}};
-        Solution3 s = new Solution3();
-        for (int i = 0; i < T; i++) print(s.solution(key[i], lock[i]));
+//        //Solution3
+//        T = 1;
+//        int[][][] key = {{{0, 0, 0}, {1, 0, 0}, {0, 1, 1}}};
+//        int[][][] lock = {{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}}};
+//        Solution3 s = new Solution3();
+//        for (int i = 0; i < T; i++) print(s.solution(key[i], lock[i]));
+//
+
+        //Solution4
+        T = 2;
+        int[] n = {5, 5};
+        int[][][] build_frame = {
+                {{1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1}},
+                {{0, 0, 0, 1}, {2, 0, 0, 1}, {4, 0, 0, 1}, {0, 1, 1, 1}, {1, 1, 1, 1}, {2, 1, 1, 1}, {3, 1, 1, 1}, {2, 0, 0, 0}, {1, 1, 1, 0}, {2, 2, 0, 1}}
+        };
+        Solution4 s = new Solution4();
+        for (int i = 0; i < T; i++) {
+            int[][] answer = s.solution(n[i], build_frame[i]);
+            for (int[] a : answer) print(a);
+        }
+
 
     }
 }
