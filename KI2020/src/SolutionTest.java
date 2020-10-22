0cef0a94457826c422bcce30312d53e53a024d0f
dreamsh19 <dreamsh19@snu.ac.kr>
2020-10-23 02:05:27 +0900

    Implement #65

--- KI2020/src/SolutionTest.java
+++ KI2020/src/SolutionTest.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class SolutionTest {
-    
+
     static void print(Object o) {
         if (o instanceof int[]) {
             System.out.println(Arrays.toString((int[]) o));
@@ -10,4 +10,18 @@ public class SolutionTest {
         }
     }
 
+    public static void main(String[] args){
+        int T;
+
+        T = 2;
+        int[][] numbers = {{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5},
+                {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2}};
+        String[] hand = {"right", "left"};
+
+        Solution1 s = new Solution1();
+        for (int i = 0; i < T; i++) {
+            print(s.solution(numbers[i], hand[i]));
+        }
+
+    }
 }
