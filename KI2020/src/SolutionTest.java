5c91ac3d576b680136184fb40d80dc32a1291bdf
dreamsh19 <dreamsh19@snu.ac.kr>
2020-10-23 02:07:29 +0900

    Implement #66

--- KI2020/src/SolutionTest.java
+++ KI2020/src/SolutionTest.java
@@ -13,15 +13,23 @@ public class SolutionTest {
     public static void main(String[] args){
         int T;
 
+//        T = 2;
+//        int[][] numbers = {{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5},
+//                {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2}};
+//        String[] hand = {"right", "left"};
+//
+//        Solution1 s = new Solution1();
+//        for (int i = 0; i < T; i++) {
+//            print(s.solution(numbers[i], hand[i]));
+//        }
+
+
         T = 2;
-        int[][] numbers = {{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5},
-                {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2}};
-        String[] hand = {"right", "left"};
+        String[] expression = {"100-200*300-500+20", "50*6-3*2"};
 
-        Solution1 s = new Solution1();
+        Solution2 s = new Solution2();
         for (int i = 0; i < T; i++) {
-            print(s.solution(numbers[i], hand[i]));
+            print(s.solution(expression[i]));
         }
-
     }
 }
