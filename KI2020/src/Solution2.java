eba552ba8c46a0042ffee7ca88303074ca11264f
dreamsh19 <dreamsh19@snu.ac.kr>
2020-12-01 06:04:42 +0900

    Refactor #66 - Modify operator type(long->char)

--- KI2020/src/Solution2.java
+++ KI2020/src/Solution2.java
@@ -4,23 +4,21 @@ import java.util.LinkedList;
 
 public class Solution2 {
 
-    static final long PLUS = '+', MINUS = '-', MULTIPLY = '*';
-
-    void swap(long[] arr, int i, int j) {
-        long tmp = arr[i];
+    void swap(char[] arr, int i, int j) {
+        char tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
     }
 
-    LinkedList<Long> operation(LinkedList<Long> arr, long operator) {
-        if (!arr.contains(operator)) return arr;
+    LinkedList<Long> operation(LinkedList<Long> arr, char operator) {
+        if (!arr.contains((long) operator)) return arr;
 
         LinkedList<Long> result = new LinkedList<>();
         Iterator<Long> it = arr.iterator();
         boolean isOperator = false;
         while (it.hasNext()) {
             long item = it.next();
-            if (isOperator && item == operator) {
+            if (isOperator && (char) item == operator) {
                 result.add(calculate(result.removeLast(), it.next(), operator));
             } else {
                 result.add(item);
@@ -31,7 +29,7 @@ public class Solution2 {
     }
 
 
-    long permute(LinkedList<Long> arr, long[] operators, int l) {
+    long permute(LinkedList<Long> arr, char[] operators, int l) {
         int n = operators.length;
         if (l == n) {
             return Math.abs(arr.getFirst());
@@ -47,33 +45,36 @@ public class Solution2 {
 
     }
 
-    long calculate(long a, long b, long operator) {
-        if (operator == PLUS) return a + b;
-        if (operator == MINUS) return a - b;
-        if (operator == MULTIPLY) return a * b;
-        throw new IllegalArgumentException("Invalid operator");
+    long calculate(long a, long b, char operator) {
+        switch (operator) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            default:
+                throw new IllegalArgumentException("Invalid operator");
+        }
     }
 
     public long solution(String expression) {
 
         LinkedList<Long> list = new LinkedList<>();
+        HashSet<Long> opSet = new HashSet<>();
         for (String exp : expression.split("\\b")) {
             long l;
             try {
                 l = Long.parseLong(exp);
             } catch (NumberFormatException e) {
                 l = exp.charAt(0);
+                opSet.add(l);
             }
             list.add(l);
         }
-        HashSet<Long> opSet = new HashSet<>();
-        for (long operator : new long[]{PLUS, MINUS, MULTIPLY}) {
-            if (list.contains(operator)) opSet.add(operator);
-        }
-        long[] operators = new long[opSet.size()];
+        char[] operators = new char[opSet.size()];
         int idx = 0;
-        for (long op : opSet) operators[idx++] = op;
-
+        for (long op : opSet) operators[idx++] = (char) op;
         return permute(list, operators, 0);
     }
 
