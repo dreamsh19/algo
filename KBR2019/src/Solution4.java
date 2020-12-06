6b50f27f361963d822d1615048b06e7c77911bd4
dreamsh19 <dreamsh19@snu.ac.kr>
2020-12-07 02:20:33 +0900

    Refactor #74

--- KBR2019/src/Solution4.java
+++ KBR2019/src/Solution4.java
@@ -6,27 +6,22 @@ public class Solution4 {
         int n = food_times.length;
         PriorityQueue<Integer> foodLeft = new PriorityQueue<>();
         for (int t : food_times) foodLeft.add(t);
-        long time = 0, elapsedTime = 0;
-        long foodEaten = 0;
-        int food;
+        int foodEaten = 0;
         while (!foodLeft.isEmpty()) {
-            food = foodLeft.peek();
-            elapsedTime = ((food - foodEaten) * foodLeft.size());
-            if (time + elapsedTime < k) {
-                time += elapsedTime;
+            int food = foodLeft.peek();
+            long timeNeedToEat = ((long) (food - foodEaten) * foodLeft.size());
+            if (timeNeedToEat <= k) {
+                k -= timeNeedToEat;
                 foodEaten = food;
                 foodLeft.remove();
             } else {
-                break;
-            }
-        }
-        if(foodLeft.isEmpty()) return -1;
-        k -= time;
-        foodEaten += (k / foodLeft.size());
-        k %= foodLeft.size();
-        for (int i = 0; i < n; i++) {
-            if (food_times[i] > foodEaten) {
-                if (k-- == 0) return i + 1;
+//                foodEaten += (k / foodLeft.size());
+                k %= foodLeft.size();
+                for (int i = 0; i < n; i++) {
+                    if (food_times[i] > foodEaten) {
+                        if (k-- == 0) return i + 1;
+                    }
+                }
             }
         }
         return -1;
