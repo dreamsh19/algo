95d8c6bf0a2506073f00317f31d6ce08870f58cf
dreamsh19 <dreamsh19@gmail.com>
2024-12-14 23:20:57 +0900

    Replace BiInput and TriInput to Pair and Triplet using javatuples library

--- NEWPROGRAMMERS/src/test/java/AbstractBiSolutionTest.java
+++ NEWPROGRAMMERS/src/test/java/AbstractBiSolutionTest.java
@@ -1,12 +1,14 @@
+import org.javatuples.Pair;
+
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-public abstract class AbstractBiSolutionTest<A1, A2, OUT> extends AbstractSolutionTest<BiInput<A1, A2>, OUT> {
+public abstract class AbstractBiSolutionTest<A1, A2, OUT> extends AbstractSolutionTest<Pair<A1, A2>, OUT> {
 
     abstract BiFunction<A1, A2, OUT> sol2();
 
     @Override
-    Function<BiInput<A1, A2>, OUT> solution() {
-        return input -> sol2().apply(input.a1, input.a2);
+    Function<Pair<A1, A2>, OUT> solution() {
+        return input -> sol2().apply(input.getValue0(), input.getValue1());
     }
 }
--- NEWPROGRAMMERS/src/test/java/AbstractTriSolutionTest.java
+++ NEWPROGRAMMERS/src/test/java/AbstractTriSolutionTest.java
@@ -1,12 +1,14 @@
+import org.javatuples.Triplet;
+
 import java.util.function.Function;
 
-public abstract class AbstractTriSolutionTest<A1, A2, A3, OUT> extends AbstractSolutionTest<TriInput<A1, A2, A3>, OUT> {
+public abstract class AbstractTriSolutionTest<A1, A2, A3, OUT> extends AbstractSolutionTest<Triplet<A1, A2, A3>, OUT> {
 
     abstract TriFunction<A1, A2, A3, OUT> sol3();
 
     @Override
-    Function<TriInput<A1, A2, A3>, OUT> solution() {
-        return input -> sol3().apply(input.a1, input.a2, input.a3);
+    Function<Triplet<A1, A2, A3>, OUT> solution() {
+        return input -> sol3().apply(input.getValue0(), input.getValue1(), input.getValue2());
     }
 
 }
