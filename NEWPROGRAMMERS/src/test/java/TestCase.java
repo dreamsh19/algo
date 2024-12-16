2c8f8014c915bfe1579af4015e84b5c85847ad6c
dreamsh19 <dreamsh19@gmail.com>
2024-12-17 00:26:05 +0900

    Add lombok for TestCase class

--- NEWPROGRAMMERS/src/test/java/TestCase.java
+++ NEWPROGRAMMERS/src/test/java/TestCase.java
@@ -1,19 +1,18 @@
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import org.javatuples.Pair;
 import org.javatuples.Triplet;
 import org.junit.jupiter.api.Assertions;
 
 import java.util.function.Function;
 
+@ToString
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class TestCase<IN, OUT> {
 
     private final IN in;
     private final OUT out;
-
-    private TestCase(IN in, OUT out) {
-        this.in = in;
-        this.out = out;
-    }
-
 
     public static <I1, O> TestCase<I1, O> of(I1 in, O out) {
         return new TestCase<>(in, out);
@@ -24,7 +23,6 @@ public class TestCase<IN, OUT> {
     }
 
     public static <I1, I2, I3, O> TestCase<Triplet<I1, I2, I3>, O> of(I1 i1, I2 i2, I3 i3, O out) {
-
         return new TestCase<>(new Triplet<>(i1, i2, i3), out);
     }
 
@@ -33,7 +31,6 @@ public class TestCase<IN, OUT> {
         OUT actual = solution.apply(this.in);
         Assertions.assertEquals(actual, this.out);
     }
-
 
 }
 
